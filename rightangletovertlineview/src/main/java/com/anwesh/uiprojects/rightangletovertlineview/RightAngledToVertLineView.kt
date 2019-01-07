9d864633b289361992fb48177c0df77578a5881a
Anwesh43 <anweshthecool0@gmail.com>
2019-01-07 08:13:18 +0530

    adding RightAngledToVertLine class

--- rightangletovertlineview/src/main/java/com/anwesh/uiprojects/rightangletovertlineview/RightAngledToVertLineView.kt
+++ rightangletovertlineview/src/main/java/com/anwesh/uiprojects/rightangletovertlineview/RightAngledToVertLineView.kt
@@ -157,4 +157,28 @@ class RightAngledToVertLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RightAngledToVerLine(var i : Int) {
+
+        private val root : RAVLNode = RAVLNode(0)
+        private var curr : RAVLNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
