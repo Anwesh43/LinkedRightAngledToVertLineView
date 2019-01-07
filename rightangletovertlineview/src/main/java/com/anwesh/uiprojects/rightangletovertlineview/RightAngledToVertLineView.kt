dde6d59ff49256893b79fd05411faa4857ef233f
Anwesh43 <anweshthecool0@gmail.com>
2019-01-07 07:54:00 +0530

    adding libraries and defining constants

--- rightangletovertlineview/src/main/java/com/anwesh/uiprojects/rightangletovertlineview/RightAngledToVertLineView.kt
+++ rightangletovertlineview/src/main/java/com/anwesh/uiprojects/rightangletovertlineview/RightAngledToVertLineView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.rightangletovertlineview
 /**
  * Created by anweshmishra on 07/01/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val line : Int = 1
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.7f
+val foreColor : Int = Color.parseColor("#1565C0")
+val backColor : Int = Color.parseColor("#BDBDBD")
