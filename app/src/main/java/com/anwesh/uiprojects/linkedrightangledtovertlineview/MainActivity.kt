ca2c322a55a5d53c12d1f30e8d469c746f8bb0d0
Anwesh43 <anweshthecool0@gmail.com>
2019-01-07 08:18:40 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedrightangledtovertlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedrightangledtovertlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedrightangledtovertlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.rightangletovertlineview.RightAngledToVertLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RightAngledToVertLineView.create(this)
     }
 }
